c0886f005ba42d3db6a5f33056e5280c653eb548
abdallahhHazem <139358688+abdallahhHazem@users.noreply.github.com>
2025-09-24 14:50:59 +0300

    Add files via upload

---
+++ Pong-Game/main.py
@@ -0,0 +1,48 @@
+import time
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("PONG")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    #detect coll with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+    #detect coll with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.pad_bounce()
+
+    if ball.xcor() > 380:
+        ball.reset_pos()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_pos()
+        scoreboard.r_point()
+
+screen.exitonclick()
